4e0c5a072c07a1dc94f3d1aa51a88d983ddd2be3
taynara <taynara.freita98@gmail.com>
2023-08-28 21:12:18 -0300

    project created

---
+++ src/main/java/com/personalproject/ratedmovies/RatedMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.personalproject.ratedmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatedMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatedMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personalproject/ratedmovies/RatedMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalproject.ratedmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatedMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
