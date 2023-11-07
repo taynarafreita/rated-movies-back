0368a1d8f060042378e599b83ab5f49b2704c70d
Taynara <taynara.freita98@gmail.com>
2023-11-07 07:54:58 -0300

    adicionando repositorio

--- src/main/java/com/personalproject/ratedmovies/repositories/MovieRepository.java
+++ src/main/java/com/personalproject/ratedmovies/repositories/MovieRepository.java
@@ -1,2 +1,10 @@
-package com.personalproject.ratedmovies.repositories;public interface MovieRepository {
+package com.personalproject.ratedmovies.repositories;
+
+import com.personalproject.ratedmovies.models.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface MovieRepository extends JpaRepository<Movie, UUID> {
+
 }
